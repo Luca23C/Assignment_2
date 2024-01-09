fb2f6a8ebfa4ec372085321d661d94bba4a54f7c
Luca23C <luca.cornia.lc@gmail.com>
2024-01-09 16:42:42 +0100

    Cancel goal updated

--- scripts/bug_ac.py
+++ scripts/bug_ac.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 import rospy
+import keyboard
 import actionlib
 import assignment_2_2023.msg
 
@@ -9,48 +10,44 @@ class ActionClient:
         rospy.init_node('bug_ac')
         self.client = actionlib.SimpleActionClient('reaching_goal', assignment_2_2023.msg.PlanningAction)
         self.client.wait_for_server()
+            
 
-    def send_goal(self, x, y):
+    def create_goal(self, x, y):
         # Construction goal message
         goal = assignment_2_2023.msg.PlanningGoal()
-        goal.target_pose.header.stamp = rospy.Time.now()
-        goal.target_pose.header.frame_id = "Rover"
         goal.target_pose.pose.position.x = x
         goal.target_pose.pose.position.y = y
-        goal.target_pose.pose.position.z = 0.0
-
-        goal.target_pose.pose.orientation.x = 0.0
-        goal.target_pose.pose.orientation.y = 0.0
-        goal.target_pose.pose.orientation.z = 0.0
-        goal.target_pose.pose.orientation.w = 1.0
 
         self.client.send_goal(goal)
         rospy.loginfo("Pose goal sent to the action server.")
 
-        # Possibility to cancel goal
-        timer = self.client.wait_for_result(rospy.Duration(30))
+        # Allow to delete goal
+        self.delete_goal()
 
-        if timer == True:
-            state = self.client.get_result()
-            rospy.loginfo("Position reached"+str(state))
 
-        else:
-            rospy.loginfo("Would you like to cancel the goal?")
-            request = str(input("Please write 'y' or 'n' "))
+    def delete_goal(self):
+        print("If you want to delete target, please enter 'y'.\nPress 'enter' to refresh terminal when robot reach the goal.")
+        request = str(input("Digit your choice: "))
+        while True:
+            timer = self.client.wait_for_result(rospy.Duration(1))
+        
+            if timer:
+                break
 
-            if request == 'y':
+            elif request == 'y':
                 self.client.cancel_goal()
+                rospy.loginfo("Goal successfully deleted!")
+                break
 
-            elif request == 'n':
-                self.client.wait_for_result()
 
     def start_interface(self):
         while not rospy.is_shutdown():
             try:
-                x = float(input("Enter X coordinate: "))
-                y = float(input("Enter Y coordinate: "))
+                x = float(input("Enter x coordinate: "))
+                y = float(input("Enter y coordinate: "))
 
-                self.send_goal(x, y)
+                self.create_goal(x, y)
+
             except ValueError:
                 rospy.logerr("Invalid input. Please provide valid numbers.")
 
