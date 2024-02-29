c55158b30c5877f9bf2c624106024bd3edfafd6b
Luca23C <luca.cornia.lc@gmail.com>
2024-02-29 11:12:17 +0100

    info_navigation_clt.py commented

--- scripts/info_navigation_clt.py
+++ scripts/info_navigation_clt.py
@@ -3,9 +3,31 @@
 import rospy
 from assignment_2_2023.srv import Navigation, NavigationResponse
 
+"""
+.. module:: assignment_2_2023
+
+    :platform: Unix
+    :synopsis: Python module for the assignment_2_2023
+
+    :moduleauthor: Luca Cornia
+
+Client:
+    /navigation_service
+"""
+
 
 def nav_client():
+    """
+    Main of the client that when called retrieve the information about the distance between the robot and the goal chosen as well as the average speed of the robot
 
+    Parameters
+    ----------
+
+    response.distance: float
+        Get the information of the distance between robot and goal
+    response.average_speed: float
+        Get the information of the average speed of the robot
+    """
     rospy.init_node('info_nav_client')
     client = rospy.ServiceProxy('/navigation_service', Navigation)
 
